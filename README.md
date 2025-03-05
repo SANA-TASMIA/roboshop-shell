d22deaab49483789388eaa9192f1bb5678cd41af
sanatasmia <sanatasmia8@gmail.com>
2025-03-05 22:30:25 +0530

    exactly same as raghu code

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # roboshop-shell
 
-testing this roboshop-shell
+testing this roboshop-shell for this
+
+dnf install unzip -y
+dnf install zip -y
+dnf install curl -y
+dnf install vim -y
+dnf install net-tools -y
--- frontend.sh
+++ frontend.sh
@@ -1,16 +1,9 @@
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
-dnf install nginx -y 
-dnf install unzip -y
-dnf install zip -y
-dnf install curl -y
-dnf install vim -y
-dnf install net-tools -y
+dnf install nginx -y
 
 cp nginx.conf /etc/nginx/nginx.conf
-
-systemctl enable nginx 
-systemctl start nginx 
+ 
 
 rm -rf /usr/share/nginx/html/* 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
@@ -19,3 +12,5 @@ cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
 systemctl restart nginx 
+systemctl enable nginx 
+systemctl status nginx
