8360cb61b5ac37a8d058e2eed53e3c3434f89ab0
sanatasmia <sanatasmia8@gmail.com>
2025-03-06 01:09:40 +0530

    changes in sed command

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ cp mongodb.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-org -y 
 
 # Update listen address from 127.0.0.1 to 0.0.0.0 in /etc/mongod.conf
-sed -i -e `s/127.0.0.0/0.0.0.0/` /etc/mongod.conf
+sed -i -e 's/127.0.0.0/0.0.0.0/' /etc/mongod.conf
 
 systemctl enable mongod 
 systemctl start mongod 
